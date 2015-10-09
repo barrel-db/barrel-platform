05533cf40ac0438a6e8ce2876a89bd46495e1473
Benoit Chesneau <bchesneau@gmail.com>
2015-10-09 10:43:30 +0900

    don't enforce roles types
    it's useful sometimes to pass plain objects as roles types, there is no real
    need type to consider a list should only be a string until we know what to
    match.

--- apps/couch/src/couch_db.erl
+++ apps/couch/src/couch_db.erl
@@ -397,9 +397,7 @@ validate_names_and_roles({Props}) when is_list(Props) ->
     _ -> throw("names must be a JSON list of strings")
     end,
     case couch_util:get_value(<<"roles">>,Props,[]) of
-    Rs when is_list(Rs) ->
-        [throw("roles must be a JSON list of strings") ||R <- Rs, not is_binary(R)],
-        Rs;
+    Rs when is_list(Rs) ->Rs;
     _ -> throw("roles must be a JSON list of strings")
     end,
     ok.
