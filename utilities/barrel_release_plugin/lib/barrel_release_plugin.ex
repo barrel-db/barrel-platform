2b601eb4b9e4b268ad51c39dd9be50575b110d76
Sushruth <sivsushruth@gmail.com>
2016-06-09 00:57:14 +0530

    Cleanup

--- utilities/barrel_release_plugin/lib/barrel_release_plugin.ex
+++ utilities/barrel_release_plugin/lib/barrel_release_plugin.ex
@@ -9,19 +9,11 @@ defmodule ReleaseManager.Plugin.BarrelRelease do
   def after_release(%Config{name: app, version: version, relx_config: relx_config} = config) do
     {result, _errcode} = System.cmd("make", ["-C", "c_src/barrel_js", "clean"])
     info result
-    IO.puts "after release"
-    #../../config/sys.config
-    IO.inspect System.cmd("pwd", [])
-    #IO.inspect File.read! "rel/#{app}/releases/#{version}/sys.config"
     BarrelRelease.SetupConfig.update_config("rel/#{app}/releases/#{version}/sys.config")
     BarrelRelease.SetupConfig.update_config("rel/#{app}/releases/#{version}/vm.args")
-    IO.inspect File.read! "rel/#{app}/releases/#{version}/sys.config"
-    #IO.inspect File.read! "rel/#{app}/releases/#{version}/sys.config"
-    #IO.inspect ReleaseManager.Config.package
   end
 
-  def after_package(%Config{name: app, version: version, relx_config: relx_config} = config) do
-    IO.inspect File.read! "rel/#{app}/releases/#{version}/sys.config"
+  def after_package(_args) do
   end
 
   def after_cleanup(_args) do
