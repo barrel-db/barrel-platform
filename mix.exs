a22b21e37e30fb594734fe2716d240a1e63e819b
Sushruth <sivsushruth@gmail.com>
2016-06-12 21:11:29 -0700

    Change mochicow tag and add parse_transform

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Barrel.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      compilers: Mix.compilers,
-     erlc_options: [:debug_info, parse_transform: :lager_transform],
+     erlc_options: [:debug_info, parse_transform: :lager_transform, parse_transform: :mochicow],
      deps: deps
    ]
   end
@@ -52,7 +52,7 @@ defmodule Barrel.Mixfile do
       {:exrm, "~> 0.18.1"},
       {:barrel_nifs, path: "utilities/barrel_release_plugin"},
       {:hooks,  "~> 1.1.1"},
-      {:mochicow, git: "https://github.com/benoitc/mochicow.git", tag: "0.5.2"},
+      {:mochicow, git: "https://github.com/benoitc/mochicow.git", tag: "0.6.0"},
       {:snappy, "~> 1.1"},
       {:ucol, "~> 2.0"},
       {:gproc, "~> 0.5.0"},
@@ -61,7 +61,7 @@ defmodule Barrel.Mixfile do
       {:hackney, "~> 1.6"},
       {:jsx, "~> 2.8.0"},
       {:stache, "~> 0.2.1"},
-      {:econfig, "~> 0.7.2"},
+      {:econfig, "~> 0.7.3"},
       {:exometer_core, "~> 1.0"}
     ]
   end
