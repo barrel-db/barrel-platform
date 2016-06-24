672695375deab17290b2e49c49cc5b408b6e5d09
Sushruth <sivsushruth@gmail.com>
2016-06-24 06:36:52 -0700

    Mkdir and touch file if not here

--- support/barrel_release_plugin/lib/barrel_release_plugin.ex
+++ support/barrel_release_plugin/lib/barrel_release_plugin.ex
@@ -8,8 +8,8 @@ defmodule ReleaseManager.Plugin.BarrelRelease do
   def after_release(%Config{name: app, version: version} = _config) do
     System.cmd("make", ["-C", "c_src/barrel_js", "clean"], into: IO.stream(:stdio, :line))
     System.cmd("escript", ["support/build_js.escript"], into: IO.stream(:stdio, :line))
-    File.mkdir!("rel/#{app}/etc")
-    File.touch!("rel/#{app}/etc/barrel.yml")
+    File.mkdir("rel/#{app}/etc")
+    File.touch("rel/#{app}/etc/barrel.yml")
     BarrelRelease.SetupConfig.update_config("config/barrel.yml", "rel/#{app}/etc/barrel.yml")
     BarrelRelease.SetupConfig.update_config("config/sys.config", "rel/#{app}/releases/#{version}/sys.config")
     BarrelRelease.SetupConfig.update_config("config/vm.args", "rel/#{app}/releases/#{version}/vm.args")
