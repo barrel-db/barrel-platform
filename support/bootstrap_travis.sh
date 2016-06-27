c23439567d0d55d3fba7a2476fab75dfe099b49e
Dan Loaiza <papachan@gmail.com>
2016-06-27 07:09:43 -0500

    use make spec to debug barrel

--- support/bootstrap_travis.sh
+++ support/bootstrap_travis.sh
@@ -4,4 +4,3 @@ curl -O -L https://s3.amazonaws.com/rebar3/rebar3
 chmod +x rebar3
 ./rebar3 update
 ./rebar3 release
-/home/travis/build/barrel-db/barrel-platform/_build/default/rel/barrel/bin/barrel start
