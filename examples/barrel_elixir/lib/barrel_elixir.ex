532dcef9ef55284c2679133c461da4bda042927a
Sushruth Sivaramakrishnan <sivsushruth@gmail.com>
2016-07-20 20:44:43 +0530

    Add example app

---
+++ examples/barrel_elixir/lib/barrel_elixir.ex
@@ -0,0 +1,2 @@
+defmodule BarrelElixir do
+end
---
+++ examples/barrel_elixir/mix.exs
@@ -0,0 +1,32 @@
+defmodule BarrelElixir.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :barrel_elixir,
+     version: "0.1.0",
+     elixir: "~> 1.3",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps()]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type "mix help compile.app" for more information
+  def application do
+    [applications: [:logger, :barrel]]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type "mix help deps" for more examples and options
+  defp deps do
+    [{:barrel, git: "https://github.com/barrel-db/barrel-platform", branch: "mix"}]
+  end
+end
