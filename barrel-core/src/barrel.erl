eee397e248653409a0e038166ac8961fb9969122
Bernard Notarianni <bernard.notarianni@gmail.com>
2016-11-08 17:03:13 +0100

    regroups api calls in barrel module

--- barrel-core/src/barrel.erl
+++ barrel-core/src/barrel.erl
@@ -18,6 +18,32 @@
 %% API
 -export([all_databases/0]).
 
+-export([
+  start/2,
+  stop/1,
+  clean/1,
+  infos/1,
+  put/4,
+  put_rev/5,
+  get/3,
+  delete/4,
+  post/3,
+  fold_by_id/4,
+  changes_since/4,
+  revsdiff/3
+]).
+
+-export([
+  attach/4,
+  attach/5,
+  get_attachment/4,
+  get_attachment_binary/4,
+  replace_attachment/5,
+  replace_attachment_binary/5,
+  delete_attachment/4,
+  attachments/3
+]).
+
 
 all_databases() ->
   {ok, Stores} = application:get_env(barrel, stores),
@@ -27,3 +53,66 @@ all_databases() ->
       Dbs ++ Acc
     end, [], Stores),
   lists:usort(AllDbs).
+
+
+start(Name, Store) ->
+  barrel_db:start(Name, Store).
+
+stop(Name) ->
+  barrel_db:stop(Name).
+
+clean(Name) ->
+  barrel_db:clean(Name).
+
+infos(Name) ->
+  barrel_db:infos(Name).
+
+get(Db, DocId, Options) ->
+  barrel_db:get(Db, DocId, Options).
+
+put(Db, DocId, Body, Options) ->
+  barrel_db:put(Db, DocId, Body, Options).
+
+put_rev(Db, DocId, Body, History, Options) ->
+  barrel_db:put_rev(Db, DocId, Body, History, Options).
+
+delete(Db, DocId, RevId, Options) ->
+  barrel_db:delete(Db, DocId, RevId, Options).
+
+post(Db, Doc, Options) ->
+  barrel_db:post(Db, Doc, Options).
+
+fold_by_id(Db, Fun, Acc, Options) ->
+  barrel_db:fold_by_id(Db, Fun, Acc, Options).
+
+revsdiff(Db, DocId, RevIds) ->
+  barrel_db:revsdiff(Db, DocId, RevIds).
+
+changes_since(Db, Since, Fun, Acc) ->
+  barrel_db:changes_since(Db, Since, Fun, Acc).
+
+
+
+attach(Db, DocId, AttDescription, Options) ->
+  barrel_attachments:attach(Db, DocId, AttDescription, Options).
+
+attach(Db, DocId, AttDescription, Binary, Options) ->
+  barrel_attachments:attach(Db, DocId, AttDescription, Binary, Options).
+
+get_attachment(Db, DocId, AttId, Options) ->
+  barrel_attachments:get_attachment(Db, DocId, AttId, Options).
+
+get_attachment_binary(Db, DocId, AttId, Options) ->
+  barrel_attachments:get_attachment_binary(Db, DocId, AttId, Options).
+
+replace_attachment(Db, DocId, AttId, AttDescription, Options) ->
+  barrel_attachments:replace_attachment(Db, DocId, AttId, AttDescription, Options).
+
+replace_attachment_binary(Db, DocId, AttId, Binary, Options) ->
+  barrel_attachments:replace_attachment_binary(Db, DocId, AttId, Binary, Options).
+
+delete_attachment(Db, DocId, AttId, Options) ->
+  barrel_attachments:delete_attachment(Db, DocId, AttId, Options).
+
+attachments(Db, DocId, Options) ->
+  barrel_attachments:attachments(Db, DocId, Options).
