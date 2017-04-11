1aadeb8c1c2848aa68374f8821f5b275f0d3710f
Bernard Notarianni <bernard.notarianni@gmail.com>
2017-04-11 12:04:00 +0200

    refactor statsd plugin
    - reuse same udp socket
    - introduce gen_server with supervisor
    
    issue #127

--- apps/barrel_store/src/barrel_stats_statsd.erl
+++ apps/barrel_store/src/barrel_stats_statsd.erl
@@ -1,4 +1,4 @@
-%% Copyright 2016, Bernard Notarianni
+%% Copyright 2017, Bernard Notarianni
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License"); you may not
 %% use this file except in compliance with the License. You may obtain a copy of
@@ -19,63 +19,98 @@
 -bahaviour(gen_server).
 
 %% plugin callbacks
--export([ init/3
+-export([ start/0
+        , init/3
         , increment/3
         , set_value/3
         , duration/3
         ]).
 
+%% gen_server callbacks
+
+-export([start_link/0]).
+-export([init/1, handle_call/3, handle_cast/2]).
+-export([handle_info/2, terminate/2, code_change/3]).
+
+
+-record(state, {socket, key}).
+
 %% Specification for statsd message format:
 %% https://github.com/etsy/statsd/blob/master/docs/metric_types.md
 
+
+start() ->
+  start_link().
+
 init(_Type, _Name, _Env) ->
+  start_link(),
   ok.
 
-set_value(Name, Value, Env) ->
-  Server = proplists:get_value(statsd_server, Env),
-  push(Server, Name, {gauge, Value}),
+set_value(Name, Value, _Env) ->
+  push(Name, {gauge, Value}),
   ok.
 
-increment(Name, Value, Env) ->
-  Server = proplists:get_value(statsd_server, Env),
-  push(Server, Name, {counter, Value}),
+increment(Name, Value, _Env) ->
+  push(Name, {counter, Value}),
   ok.
 
-duration(Name, Value, Env) ->
-  Server = proplists:get_value(statsd_server, Env),
-  push(Server, Name, {duration, Value}),
+duration(Name, Value, _Env) ->
+  push(Name, {duration, Value}),
   ok.
 
-push(Server, MetricName, Value) ->
+push(MetricName, Value) ->
+  gen_server:cast(?MODULE, {send, MetricName, Value}).
+
+
+%% gen_server api
+
+start_link() ->
+  gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+%%% Server functions
+init(_) ->
+  Env = application:get_env(barrel_store, metrics, undefined),
+  {Peer, Port} = proplists:get_value(statsd_server, Env),
   [Node, Host] = binary:split(atom_to_binary(node(), utf8), <<"@">>),
   HostWithoutDots = barrel_lib:binary_join(binary:split(Host, <<".">>, [global]), <<"_">>),
-  StatsdKey = barrel_lib:binary_join([<<"barrel">>, HostWithoutDots, Node|MetricName], <<".">>),
-  send(Server, StatsdKey, Value).
+  StatsdKey = barrel_lib:binary_join([<<"barrel">>, HostWithoutDots, Node], <<".">>),
+  {ok, Socket} = gen_udp:open(0),
+  lager:info("init statsd metrics peer=~p port=~p", [Peer, Port]),
+  {ok, #state{socket={Socket, Peer, Port}, key=StatsdKey}}.
 
-send({Peer, Port}, Key, {counter, Value}) ->
+handle_call(terminate, _From, State) ->
+  {stop, normal, ok, State}.
+
+handle_cast({send, MetricName, Value}, #state{socket=Socket, key=ServerKey}=State) ->
+  MetricJoined = barrel_lib:binary_join(MetricName, <<".">>),
+  FullKey = barrel_lib:binary_join([ServerKey, MetricJoined], <<".">>),
+  send(Socket, FullKey, Value),
+  {noreply, State}.
+
+handle_info(_, State) ->
+  {noreply, State}.
+
+terminate(normal, _State) ->
+  ok.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+send(Socket, Key, {counter, Value}) ->
   BVal = integer_to_binary(Value),
   Data = <<Key/binary, ":", BVal/binary, "|c">>,
-  udp(Peer, Port, Data);
+  udp(Socket, Data);
 
-send({Peer, Port}, Key, {gauge, Value}) ->
+send(Socket, Key, {gauge, Value}) ->
   BVal = integer_to_binary(Value),
   Data = <<Key/binary, ":", BVal/binary, "|g">>,
-  udp(Peer, Port, Data);
+  udp(Socket, Data);
 
-send({Peer, Port}, Key, {duration, Value}) ->
+send(Socket, Key, {duration, Value}) ->
   BVal = integer_to_binary(Value),
   Data = <<Key/binary, ":", BVal/binary, "|ms">>,
-  udp(Peer, Port, Data).
+  udp(Socket, Data).
 
-udp(Peer, Port, Data) ->
-  Fun = fun() ->
-            case gen_udp:open(0) of
-              {ok, Socket} ->
-                _ = gen_udp:send(Socket, Peer, Port, Data),
-                gen_udp:close(Socket);
-              Error ->
-                lagger:error("can not open udp socket to statsd server: ~p", [Error])
-            end
-        end,
-  _ = spawn(Fun),
+udp({Socket, Peer, Port}, Data) ->
+  _ = gen_udp:send(Socket, Peer, Port, Data),
   ok.
